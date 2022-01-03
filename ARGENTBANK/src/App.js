af74312d0ebe3c8cdbffd7c82e6ff3a52edc9bae
pedro VINAGRE <pedro.vinagre@expleogroup.com>
2022-01-03 13:32:49 +0100

    suite

--- ARGENTBANK/src/App.js
+++ ARGENTBANK/src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { BrowserRouter} from 'react-router-dom'
+// import Router from './router/Router'
+import Header from './components/Header'
+import Footer from './components/Footer'
+// import Home from './pages/Home'
+import SignIn from './pages/sign-in'
+// import User from './pages/user'
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className='ArgentBank'>
+      <BrowserRouter>
+        <Header />
+        {/* <Router /> */}
+        {/* <Home /> */}
+        <SignIn />
+        {/* <User /> */}
+        <Footer />
+      </BrowserRouter>
+      </div>
   );
 }
 
